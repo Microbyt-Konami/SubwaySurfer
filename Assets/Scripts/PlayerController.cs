34adb41f6a5861222a9e0d3929ea6bfde76be449
bugbit <ohernandezba@gmail.com>
2024-01-15 22:57:11 +0100

    Movimiento derecha, izquierda y salto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,32 @@ public enum PlayerPosition { Left = -2, Middle = 0, Right = 2 }
 
 public class PlayerController : MonoBehaviour
 {
+    // Fields
+    [SerializeField] private float dodgeSpeed;
+    [SerializeField] private float jumpPower = 7;
+
+    // Components
     private PlayerPosition playerPosition;
     private Transform playerTransform;
-    private bool swipeLeft, swipeRight;
-    private float newXPosition;
+    private Animator playerAnimation;
+    private CharacterController characterController;
+
+    // Variables
+    private bool swipeLeft, swipeRight, swipeUp, swipeDown;
+    private float newXPosition, xPosition, yPosition;
+    private Vector3 motionVector;
+
+    // Ids
+    private int idDodgeLeft = Animator.StringToHash("DodgeLeft");
+    private int idDodgeRight = Animator.StringToHash("DodgeRight");
 
     // Start is called before the first frame update
     void Start()
     {
         playerPosition = PlayerPosition.Middle;
         playerTransform = GetComponent<Transform>();
+        playerAnimation = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -30,6 +46,7 @@ public class PlayerController : MonoBehaviour
     {
         swipeLeft = Input.GetKeyDown(KeyCode.LeftArrow);
         swipeRight = Input.GetKeyDown(KeyCode.RightArrow);
+        swipeUp = Input.GetKeyDown(KeyCode.UpArrow);
     }
 
     private void SetPlayerPosition()
@@ -40,9 +57,11 @@ public class PlayerController : MonoBehaviour
             {
                 case PlayerPosition.Middle:
                     UpdatePlayerXPosition(PlayerPosition.Left);
+                    SetPlayerAnimator(idDodgeLeft);
                     break;
                 case PlayerPosition.Right:
                     UpdatePlayerXPosition(PlayerPosition.Middle);
+                    SetPlayerAnimator(idDodgeLeft);
                     break;
             }
         }
@@ -52,13 +71,16 @@ public class PlayerController : MonoBehaviour
             {
                 case PlayerPosition.Middle:
                     UpdatePlayerXPosition(PlayerPosition.Right);
+                    SetPlayerAnimator(idDodgeRight);
                     break;
                 case PlayerPosition.Left:
                     UpdatePlayerXPosition(PlayerPosition.Middle);
+                    SetPlayerAnimator(idDodgeRight);
                     break;
             }
         }
         MovePlayer();
+        Jump();
     }
 
     private void UpdatePlayerXPosition(PlayerPosition position)
@@ -67,8 +89,31 @@ public class PlayerController : MonoBehaviour
         playerPosition = position;
     }
 
+    private void SetPlayerAnimator(int id)
+    {
+        playerAnimation.Play(id);
+    }
+
     private void MovePlayer()
     {
-        playerTransform.position = new Vector3(newXPosition, 0, 0);
+        motionVector = new Vector3(xPosition - playerTransform.position.x, yPosition * Time.deltaTime, 0);
+        xPosition = Mathf.Lerp(xPosition, newXPosition, Time.deltaTime * dodgeSpeed);
+        //playerTransform.position = new Vector3(xPosition, 0, 0);
+        // cuidado es relativo no absoluto
+        characterController.Move(motionVector);
+    }
+
+    private void Jump()
+    {
+        if (characterController.isGrounded)
+        {
+            if (swipeUp)
+                yPosition = jumpPower;
+        }
+        else
+        {
+            // Se baja mas rápido
+            yPosition -= jumpPower * 2 * Time.deltaTime;
+        }
     }
 }
