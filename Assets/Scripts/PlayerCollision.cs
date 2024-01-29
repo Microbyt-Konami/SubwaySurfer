d0bf9753851fdd80f513cf31b597a8fecc1df9ce
bugbit <ohernandezba@gmail.com>
2024-01-29 22:38:19 +0100

    Finalizando sistema de colisiones en X

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -12,38 +12,40 @@ public class PlayerCollision : MonoBehaviour
     // Componentes
     private PlayerController playerController;
 
-    public void OnCharacterColission(Collider collider) => CalcCollisionX(collider);
+    public void OnCharacterColission(Collider collider) => collisionX = CalcCollisionX(collider);
 
     private void Awake()
     {
         playerController = gameObject.GetComponent<PlayerController>();
     }
 
-    private void CalcCollisionX(Collider collider)
+    private CollisionX CalcCollisionX(Collider collider)
     {
         // Collider Bounds obtiene los valores del tamaño de collider BOUNDS == LIMITE
         Bounds characterControllerBounds = playerController.MyCharacterController.bounds;
         Bounds colliderBounds = collider.bounds;
         // obtener el punto máximo de los mínimos de X
-        float minX = Mathf.Max(characterControllerBounds.min.x, colliderBounds.min.x);
+        float minX = Mathf.Max(colliderBounds.min.x, characterControllerBounds.min.x);
         // obtener el punto máximo de los mínimos de X
-        float maxX = Mathf.Min(characterControllerBounds.max.x, colliderBounds.max.x);
+        float maxX = Mathf.Min(colliderBounds.max.x, characterControllerBounds.max.x);
         // SABER EL PUNTO EXACTO DONDE COLISIONA UN OBJETO
 
         // Al mínimo sumarle el máximo y obtener el total
         // Lo divido por 2 así estaré en la mitad de la colisión o pivote
         // Se lo resto al total de collider del que necesite obtener la posició exacta
 
-        float average = (minX + maxX) / 2 - characterControllerBounds.min.x;
+        float average = (minX + maxX) / 2 - colliderBounds.min.x;
 
         // Como lo divimos en tres partes 1/3=0.33
         const float fraccion = 1.0f / 3.0f;
 
-        if (average > colliderBounds.size.x - fraccion)
-            collisionX = CollisionX.Right;
-        else if (average < fraccion)
-            collisionX = CollisionX.Left;
-        else
-            collisionX = CollisionX.Middle;
+        CollisionX colX =
+            (average > colliderBounds.size.x - fraccion)
+                ? CollisionX.Right
+                : (average < fraccion)
+                    ? CollisionX.Left
+                    : CollisionX.Middle;
+
+        return colX;
     }
 }
